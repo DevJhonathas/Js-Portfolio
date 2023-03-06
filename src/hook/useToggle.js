b6bb079ef8c4e4a3b594bc7c9febc5147d5694b8
Jhonathas <jhonathassouza8@gmail.com>
2023-03-06 12:00:10 -0300

    melhoria do código do  ToggleButton

--- src/hook/useToggle.js
+++ src/hook/useToggle.js
@@ -1,24 +1,22 @@
 import '../index.css';
 import { NavLink } from 'react-router-dom';
 import menu from "../img/menu_white_36dp.svg"
+import { useState } from 'react'; 
 
 export function ToggleButton(){
-    function menuShow() {
-        let menuMobile = document.querySelector('.nav_list_mobile');
-        if (menuMobile.classList.contains('open')){
-            menuMobile.classList.remove('open');
-        } else {
-            menuMobile.classList.add('open');
-        }
+
+    const [contains, setContains] = useState(false);
+    const ToggleMode = () => {
+        setContains(!contains);
     }
     
     return (
         <>
             <div className="moblie_menu_icon">
-                <button onClick={menuShow}><img src={menu} alt="burguer" /></button>
+                <button onClick={ToggleMode}><img src={menu} alt="burguer" /></button>
             </div>
 
-            <div className='nav_list_mobile'>
+            <div className={contains ? 'nav_list_mobile open' : 'nav_list_mobile'}>
                 <ul className='link_list_mobile'>
                     <li>
                         <NavLink to="/" className={({isActive}) => (isActive ? 'active' : "")}>
