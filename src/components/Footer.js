5ccf696b322d64127c3a96c281e8724b4c94b98d
Jhonathas <jhonathassouza8@gmail.com>
2023-03-14 00:18:40 -0300

    Modificações no home, root do css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <ul className={styles.navbar_footer}>
             <li><NavLink to='/' className={({isActive}) => (isActive ? styles.active : "")}>Home</NavLink></li>
             <li><NavLink to='/projects' className={({isActive}) => (isActive ? styles.active : "")}>Projetos</NavLink></li>
-            <li><NavLink to='/about' className={({isActive}) => (isActive ? styles.active : "")}>About</NavLink></li>
+            <li><NavLink to='/about' className={({isActive}) => (isActive ? styles.active : "")}>Sobre</NavLink></li>
             <li><NavLink to='/contact' className={({isActive}) => (isActive ? styles.active : "")}>Contate-me</NavLink></li>
         </ul>
       </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import styles from "./Home.module.css";
+import computerHand from "../../img/HandComputer.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <>
+      <div className={styles.title_container}>
+        <h1>Introdução:</h1>
+        <div className={styles.subtitle}>
+        <p>Bem vindo ao meu <span>Portfolio</span>.</p>
+        <p>Aqui irei mostrar alguns projetos e falar um pouco sobre mim.</p>
+        </div>
+      </div>
+      <main className={styles.main_container}>
+          <div className={styles.image_svg}>
+            <img src={computerHand} alt="Hand typing on computer" />
+           </div>
+          <div className={styles.about_me}>
+              <div className={styles.myself}>
+                <img src="" alt="Minha foto" />
+              </div>
+              <div className={styles.about}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quis rerum ipsam debitis qui, sapiente architecto unde facilis nemo cupiditate soluta aliquam dolores deleniti perferendis tenetur impedit error iure et.</p>
+              </div>
+          </div>
+      </main>
+
+    </>
   )
 }
 
